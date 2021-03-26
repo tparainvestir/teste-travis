c78c15f75c651abc5353afb3d13b08152ae6ebc9
tparainvestir <tparainvestir@gmail.com>
2021-03-25 21:41:14 -0300

    Versão Inicial do Projeto

---
+++ src/main/java/com/tempoparainvestir/api/TesteTravisApplication.java
@@ -0,0 +1,13 @@
+package com.tempoparainvestir.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteTravisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteTravisApplication.class, args);
+	}
+
+}
